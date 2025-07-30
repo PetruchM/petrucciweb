d811906b1b56e8bfa5f99d7e40fba7af32c2c3f3
PetruchN <nickpetruch@gmail.com>
2025-07-30 11:00:25 +0200

    test

--- slideshow.js
+++ slideshow.js
@@ -17,3 +17,4 @@ setInterval(() => {
 index = (index + 1) % images.length; // přejde zpět na začátek po posledním
 document.getElementById("slideshow").src = images[index];
 }, 2000); // každou sekundu
+// něco cokoli
